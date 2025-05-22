2a0d5d3f7540c187a0aecf361d96781966b7ea2b
Akai <nguyensieu12112002@gmail.com>
2025-05-22 19:34:15 +0700

    Create ValuesController.cs

---
+++ DPTS.APIs/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DPTS.APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> actionResult()
+        {
+            return Created();
+            //return Ok();
+        }
+    }
+}
