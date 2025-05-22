c156428690f47282016f7e6509a1be462f8909f9
Akai <nguyensieu12112002@gmail.com>
2025-05-22 19:34:27 +0700

    Create StatisticsNumberOfUserModel.cs

---
+++ DPTS.Applications/Dtos/StatisticsNumberOfUserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DPTS.Applications.Dtos
+{
+    public class StatisticsNumberOfUserModel
+    {
+        public string RoleName { get; set; } = string.Empty;
+        public int Quantity {  get; set; }
+    }
+}
