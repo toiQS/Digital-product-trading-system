8ed7521c31bc359dddd1679ddfabfe48b47ac243
Akai <nguyensieu12112002@gmail.com>
2025-05-22 19:34:42 +0700

    Create OrderItem.cs

---
+++ DPTS.Domains/OrderItem.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DPTS.Domains
+{
+    public class OrderItem
+    {
+        [Key]
+        [Column("order_item_id")]
+        public string OrderItemId { get; set; } = string.Empty;
+        [Column("product_id")]
+        public string ProductId { get; set; } = string.Empty;
+
+        [Column("quantity")]
+        public int Quantity { get; set; } = 1;
+
+        [Column("total_amount")]
+        public decimal TotalAmount { get; set; }
+
+        [Column("order_id")]
+        public string OrderId { get; set; } = string.Empty;
+
+        public virtual Product Product { get; set; } = null!;
+        public virtual Order Order { get; set; } = null!;
+    }
+}
