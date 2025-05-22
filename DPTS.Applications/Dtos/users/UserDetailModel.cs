551a7f3d8ca846cbb3fd34eb0aa38b116ca29e69
Akai <nguyensieu12112002@gmail.com>
2025-05-22 19:34:29 +0700

    Create UserDetailModel.cs

---
+++ DPTS.Applications/Dtos/users/UserDetailModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DPTS.Applications.Dtos.users
+{
+    public class UserDetailModel
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string NumberPhone { get; set; } = string.Empty;
+        public string FullName {  get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string RoleName {  get; set; } = string.Empty;
+    }
+}
