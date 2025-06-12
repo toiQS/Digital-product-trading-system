57c63f54096adf504f975a1ee21f36c422bd58b1
Akai <nguyensieu12112002@gmail.com>
2025-06-13 03:12:05 +0700

    Create RecentOrderDto.cs

---
+++ DPTS.Applications/Seller/dashbroads/Dtos/RecentOrderDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DPTS.Applications.Seller.dashbroads.Dtos
+{
+    public class RecentOrderDto
+    {
+        public string CustomerName { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public string Time { get; set; } = string.Empty;
+    }
+}
