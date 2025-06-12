bf3a792d899319987eef97b210c517f6ee4c45f8
Akai <nguyensieu12112002@gmail.com>
2025-06-13 03:13:58 +0700

    Create IOrderItemRepository.cs

---
+++ DPTS.Infrastructures/Repository/Interfaces/IOrderItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using DPTS.Domains;
+
+namespace DPTS.Infrastructures.Repository.Interfaces
+{
+    public interface IOrderItemRepository
+    {
+        Task AddAsync(OrderItem item);
+        Task DeleteAsync(string id);
+        Task<OrderItem?> GetByIdAsync(string id, bool includeProduct = false, bool includeOrder = false);
+        Task<IEnumerable<OrderItem>> GetsAsync(string? orderId = null, string? productId = null, double? minAmount = null, double? maxAmount = null, int? minQuantity = null, int? maxQuantity = null, bool includeProduct = false, bool includeOrder = false);
+        Task UpdateAsync(OrderItem item);
+    }
+}
