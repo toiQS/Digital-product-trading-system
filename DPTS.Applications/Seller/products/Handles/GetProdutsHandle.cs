dcba416de3657b2a30226840b9e58b4cd63a70e9
Akai <nguyensieu12112002@gmail.com>
2025-06-13 03:14:07 +0700

    Create GetProdutsHandle.cs

---
+++ DPTS.Applications/Seller/products/Handles/GetProdutsHandle.cs
@@ -0,0 +1,18 @@
+﻿using DPTS.Applications.Seller.products.Dtos;
+using DPTS.Applications.Seller.products.Queries;
+using DPTS.Applications.Shareds;
+using DPTS.Infrastructures.Repository.Interfaces;
+using Org.BouncyCastle.Asn1.X500.Style;
+using System.Threading.Tasks;
+
+namespace DPTS.Applications.Seller.products.Handles
+{
+    public class GetProdutsHandle
+    {
+        private readonly IProductRepository _productRepository;
+        //public async Task<ServiceResult<IEnumerable<ProductListItemDto>>> ExcuteAsync(GetProductsItemQuery query)
+        //{
+        //    var prouduts = await _productRepository.GetsAsync();
+        //}
+    }
+}
