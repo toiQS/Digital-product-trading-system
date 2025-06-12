5161934534d2695be3d5ea3392481920211583ef
Akai <nguyensieu12112002@gmail.com>
2025-06-13 03:14:02 +0700

    Update Category.cs

--- DPTS.Domains/Category.cs
+++ DPTS.Domains/Category.cs
@@ -7,13 +7,17 @@ namespace DPTS.Domains
     {
         [Key]
         [Column("category_id")]
-        public string CategoryId {  get; set; } = string.Empty;
+        public string CategoryId { get; set; } = string.Empty;
+
         [Column("category_name")]
         public string CategoryName { get; set; } = string.Empty;
+
         [Column("category_icon")]
         public string CategoryIcon { get; set; } = string.Empty;
+
         [Column("create_at")]
         public DateTime CreateAt { get; set; }
-        public List<Product> Products { get; set; } = new List<Product>();
+
+        public List<Product> Products { get; set; } = new();
     }
 }
