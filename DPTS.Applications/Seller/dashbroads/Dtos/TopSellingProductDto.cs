537151166987c9c9e815ffbba3e74223a1c8b808
Akai <nguyensieu12112002@gmail.com>
2025-06-13 03:12:12 +0700

    Create TopSellingProductDto.cs

---
+++ DPTS.Applications/Seller/dashbroads/Dtos/TopSellingProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DPTS.Applications.Seller.dashbroads.Dtos
+{
+    public class TopSellingProductDto
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public string CategoryCode { get; set; } = string.Empty; // UI, PH, etc.
+        public int QuantitySold { get; set; }
+        public decimal TotalRevenue { get; set; }
+    }
+}
