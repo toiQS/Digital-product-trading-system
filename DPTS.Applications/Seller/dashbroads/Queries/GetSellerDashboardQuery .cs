fa5c351977e6d3d87df2f73b69eb9ea7ce6bbcfb
Akai <nguyensieu12112002@gmail.com>
2025-06-13 03:14:12 +0700

    Create GetSellerDashboardQuery .cs

---
+++ DPTS.Applications/Seller/dashbroads/Queries/GetSellerDashboardQuery .cs
@@ -0,0 +1,14 @@
+﻿using DPTS.Applications.Seller.dashbroads.Dtos;
+using MediatR;
+
+namespace DPTS.Applications.Seller.dashbroads.Queries
+{
+    public class GetSellerDashboardQuery : IRequest<SellerDashboardDto>
+    {
+        public string SellerId { get; set; } = string.Empty;
+        public GetSellerDashboardQuery(string selelrId)
+        {
+            SellerId = selelrId;
+        }
+    }
+}
