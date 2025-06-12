b171a24d977768ab6f13bb76a87bf9e7f8fffdd6
Akai <nguyensieu12112002@gmail.com>
2025-06-13 03:13:06 +0700

    Create IComplaintImageRepository.cs

---
+++ DPTS.Infrastructures/Repository/Interfaces/IComplaintImageRepository.cs
@@ -0,0 +1,13 @@
+﻿using DPTS.Domains;
+
+namespace DPTS.Infrastructures.Repository.Interfaces
+{
+    public interface IComplaintImageRepository
+    {
+        Task AddAsync(ComplaintImage image);
+        Task DeleteAsync(string id);
+        Task<IEnumerable<ComplaintImage>> GetByComplaintIdAsync(string complaintId);
+        Task<ComplaintImage?> GetByIdAsync(string id);
+        Task<IEnumerable<ComplaintImage>> GetsAsync(string? complaintId = null, string? keyword = null, bool includeComplaint = false);
+    }
+}
