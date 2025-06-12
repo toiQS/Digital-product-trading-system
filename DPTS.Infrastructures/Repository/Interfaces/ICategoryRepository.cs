af35baa3b01f36ee2e2594b012725f20d1526213
Akai <nguyensieu12112002@gmail.com>
2025-06-13 03:13:04 +0700

    Create ICategoryRepository.cs

---
+++ DPTS.Infrastructures/Repository/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using DPTS.Domains;
+
+namespace DPTS.Infrastructures.Repository.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task AddAsync(Category category);
+        Task DeleteAsync(string id);
+        Task<Category?> GetByIdAsync(string id);
+        Task<IEnumerable<Category>> GetsAsync(string? text = null, bool includeProduct = true);
+        Task UpdateAsync(Category category);
+    }
+}
