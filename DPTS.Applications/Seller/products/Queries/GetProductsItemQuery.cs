cd67f432c99e3b8015c627c5846af03b5806a63f
Akai <nguyensieu12112002@gmail.com>
2025-06-13 03:12:18 +0700

    Create GetProductsItemQuery.cs

---
+++ DPTS.Applications/Seller/products/Queries/GetProductsItemQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace DPTS.Applications.Seller.products.Queries
+{
+    public class GetProductsItemQuery : IRequest<GetProductsItemQuery>
+    {
+        public string SellerId { get; set; } = string.Empty;
+        public int PageSize { get; set; }
+        public int PageCount { get; set; }
+    }
+}
