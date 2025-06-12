cc1eb85e081e94ef16cdadf164cf29b4acdc3b79
Akai <nguyensieu12112002@gmail.com>
2025-06-13 03:12:08 +0700

    Create RevenueChartDto.cs

---
+++ DPTS.Applications/Seller/dashbroads/Dtos/RevenueChartDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DPTS.Applications.Seller.dashbroads.Dtos
+{
+    public class RevenueChartDto
+    {
+        public string Label { get; set; } = string.Empty;  // e.g. T2, T3, T4...
+        public decimal Revenue { get; set; }
+    }
+}
