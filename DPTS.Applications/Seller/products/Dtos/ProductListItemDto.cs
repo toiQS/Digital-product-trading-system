c5c4d77208c440f1edf8a7884c5a1cb846048343
Akai <nguyensieu12112002@gmail.com>
2025-06-13 03:14:10 +0700

    Create ProductListItemDto.cs

---
+++ DPTS.Applications/Seller/products/Dtos/ProductListItemDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DPTS.Applications.Seller.products.Dtos
+{
+    public class ProductListItemDto
+    {
+        public string ProductId { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty; // e.g. Ebook, Khóa học
+        public decimal Price { get; set; }
+        public int SoldCount { get; set; }
+        public string Status { get; set; } = string.Empty; // "Hoạt động", "Đang chờ", "Bị chặn"
+        public string ProductImage { get; set; } = string.Empty;
+    }
+}
