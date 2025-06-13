8655598311c30bc6ff6c2ed107da781bd1b7ab3f
Akai <nguyensieu12112002@gmail.com>
2025-06-14 04:10:13 +0700

    Update SellerDashboardDto.cs

--- DPTS.Applications/Seller/dashbroads/Dtos/SellerDashboardDto.cs
+++ DPTS.Applications/Seller/dashbroads/Dtos/SellerDashboardDto.cs
@@ -5,7 +5,7 @@
         public decimal TotalRevenue { get; set; }
         public int TotalOrders { get; set; }
         public int TotalProducts { get; set; }
-        public double AverageRating { get; set; }
+        public decimal AverageRating { get; set; }
 
         public List<RevenueChartDto> RevenueChart { get; set; } = new List<RevenueChartDto>();
         public List<TopSellingProductDto> TopSellingProducts { get; set; } = new List<TopSellingProductDto>();
