b912421b1eba7237cff9cba915c9ddd2d9d77aa8
Akai <nguyensieu12112002@gmail.com>
2025-06-14 04:10:16 +0700

    Update Config.cs

--- DPTS.Applications/Config.cs
+++ DPTS.Applications/Config.cs
@@ -1,7 +1,10 @@
 ﻿using DPTS.Infrastructures.Data;
+using DPTS.Infrastructures.Repository.Implements;
+using DPTS.Infrastructures.Repository.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace DPTS.Applications
 {
@@ -10,6 +13,7 @@ namespace DPTS.Applications
         public static void Initalize(this IServiceCollection services, IConfiguration configuration)
         {
             services.InitializeConnect(configuration);
+            services.InitializeRepository();
             services.InitializeService();
         }
         private static void InitializeConnect(this IServiceCollection services, IConfiguration configuration)
@@ -19,14 +23,23 @@ namespace DPTS.Applications
         private static void InitializeService(this IServiceCollection services)
         {
 
-            //services.AddScoped<IStatisticService, StatictisService>();
-            //services.AddScoped<IAuthService, AuthService>();
-            //services.AddScoped<IMessageService, MessageService>();
-            //services.AddScoped<ICategoryService, CategoryService>();
-            //services.AddScoped<IOrderService, OrderService>();
-            //services.AddScoped<IProductService, ProductService>();
-            //services.AddScoped<IUserService, UserService>();
-            //services.AddScoped<IWalletService, WalletService>();
+        }
+        private static void InitializeRepository(this IServiceCollection services)
+        {
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IComplaintRepository, ComplaintRepository>();
+            services.AddScoped<IComplaintImageRepository, ComplaintImageRepository>();
+            services.AddScoped<IEscrowRepository, EscrowRepository>();
+            services.AddScoped<ILogRepository, LogRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
+            services.AddScoped<IOrderItemRepository, OrderItemRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductImageRepository, ProductImageRepository>();
+            services.AddScoped<IProductReviewRepository, ProductReviewRepository>();
+            services.AddScoped<ITradeRepository, TradeRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IWalletRepository, WalletRepository>();
         }
     }
 }
