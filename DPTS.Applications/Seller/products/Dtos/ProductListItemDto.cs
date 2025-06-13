e46acd87686ab4d533966c65538bd0faa313d75e
Akai <nguyensieu12112002@gmail.com>
2025-06-14 04:10:18 +0700

    Update ProductListItemDto.cs

--- DPTS.Applications/Seller/products/Dtos/ProductListItemDto.cs
+++ DPTS.Applications/Seller/products/Dtos/ProductListItemDto.cs
@@ -2,12 +2,14 @@
 {
     public class ProductListItemDto
     {
+
         public string ProductId { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
         public string ProductName { get; set; } = string.Empty;
-        public string Category { get; set; } = string.Empty; // e.g. Ebook, Khóa học
+        public string Category { get; set; } = string.Empty;
+        public int QuantitySelled { get; set; }
         public decimal Price { get; set; }
-        public int SoldCount { get; set; }
-        public string Status { get; set; } = string.Empty; // "Hoạt động", "Đang chờ", "Bị chặn"
-        public string ProductImage { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public double Rating { get; set; }
     }
 }
