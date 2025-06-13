b8ad1a897d15a89579ea9081794026ec9bd36602
Akai <nguyensieu12112002@gmail.com>
2025-06-14 04:10:23 +0700

    Update GetProductsItemQuery.cs

--- DPTS.Applications/Seller/products/Queries/GetProductsItemQuery.cs
+++ DPTS.Applications/Seller/products/Queries/GetProductsItemQuery.cs
@@ -1,8 +1,10 @@
-﻿using MediatR;
+﻿using DPTS.Applications.Seller.products.Dtos;
+using DPTS.Applications.Shareds;
+using MediatR;
 
 namespace DPTS.Applications.Seller.products.Queries
 {
-    public class GetProductsItemQuery : IRequest<GetProductsItemQuery>
+    public class GetProductsItemQuery : IRequest<ServiceResult<IEnumerable<ProductListItemDto>>>
     {
         public string SellerId { get; set; } = string.Empty;
         public int PageSize { get; set; }
