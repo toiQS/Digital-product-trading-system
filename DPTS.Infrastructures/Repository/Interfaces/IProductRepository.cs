00ad613661e5b3e3cf054b45f790ca8586869e09
Akai <nguyensieu12112002@gmail.com>
2025-06-14 14:38:18 +0700

    Update IProductRepository.cs

--- DPTS.Infrastructures/Repository/Interfaces/IProductRepository.cs
+++ DPTS.Infrastructures/Repository/Interfaces/IProductRepository.cs
@@ -7,7 +7,7 @@ namespace DPTS.Infrastructures.Repository.Interfaces
         Task AddAsync(Product product);
         Task DeleteAsync(string id);
         Task<Product?> GetByIdAsync(string id, bool includeCategory = false, bool includeImages = false, bool includeReviews = false);
-        Task<IEnumerable<Product>> GetsAsync(string? sellerId = null, string? categoryId = null, ProductStatus? status = null, decimal? minPrice = null, decimal? maxPrice = null, string? keyword = null, bool includeCategory = false, bool includeImages = false, bool includeReviews = false);
+        Task<IEnumerable<Product>> GetsAsync(string? sellerId = null,string? text = null, string? categoryId = null, ProductStatus? status = null, decimal? minPrice = null, decimal? maxPrice = null, string? keyword = null, bool includeCategory = false, bool includeImages = false, bool includeReviews = false);
         Task UpdateAsync(Product product);
     }
 }
