f880cb32e5d6090defff6c87e8031822dc0e9081
Akai <nguyensieu12112002@gmail.com>
2025-06-16 11:11:55 +0700

    Delete RevenueChartDto.cs

--- DPTS.Applications/Seller/dashbroads/Dtos/RevenueChartDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DPTS.Applications.Seller.dashbroads.Dtos
-{
-    public class RevenueChartDto
-    {
-        public string Label { get; set; } = string.Empty;  // e.g. T2, T3, T4...
-        public decimal Revenue { get; set; }
-    }
-}
