038252bf17bc270e2a65c27d35a0c04e24e46ae6
Akai <nguyensieu12112002@gmail.com>
2025-06-16 11:11:50 +0700

    Delete RecentMessageDto.cs

--- DPTS.Applications/Seller/dashbroads/Dtos/RecentMessageDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DPTS.Applications.Seller.dashbroads.Dtos
-{
-    public class RecentMessageDto
-    {
-        public string CustomerName { get; set; } = string.Empty;
-        public string ContentSnippet { get; set; } = string.Empty;
-        public string TimeAgo { get; set; } = string.Empty; // "Hôm qua", "12/05", etc.
-    }
-}
