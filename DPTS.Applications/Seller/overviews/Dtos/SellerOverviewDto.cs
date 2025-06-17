31778da47fa384bd1f87894fb9211849cef0c235
Akai <nguyensieu12112002@gmail.com>
2025-06-17 22:17:02 +0700

    Create SellerOverviewDto.cs

---
+++ DPTS.Applications/Seller/overviews/Dtos/SellerOverviewDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DPTS.Applications.Seller.overviews.Dtos
+{
+    public class SellerOverviewDto
+    {
+        public string OverViewName { get; set; } = string.Empty;
+        public string Information { get; set; } = string.Empty;
+        public decimal Value { get; set; }
+    }
+}
