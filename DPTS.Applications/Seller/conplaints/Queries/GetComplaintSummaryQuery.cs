dacffcf392d1786961edb62b5e618f89a57ecf2c
Akai <nguyensieu12112002@gmail.com>
2025-06-17 22:17:00 +0700

    Create GetComplaintSummaryQuery.cs

---
+++ DPTS.Applications/Seller/conplaints/Queries/GetComplaintSummaryQuery.cs
@@ -0,0 +1,11 @@
+﻿using DPTS.Applications.Seller.conplaints.Dtos;
+using DPTS.Applications.Shareds;
+using MediatR;
+
+namespace DPTS.Applications.Seller.conplaints.Queries
+{
+    public class GetComplaintSummaryQuery : IRequest<ServiceResult<IEnumerable<ComplaintSummaryDto>>>
+    {
+        public string SellerId { get; set; } = string.Empty;
+    }
+}
