c8af48f477f3f3ba266061b19850e606f123d18a
Akai <nguyensieu12112002@gmail.com>
2025-06-18 16:19:23 +0700

    Create GetSellerOverviewQuery.cs

---
+++ DPTS.Applications/Seller/overviews/Queries/GetSellerOverviewQuery.cs
@@ -0,0 +1,12 @@
+﻿using DPTS.Applications.Seller.overviews.Dtos;
+using DPTS.Applications.Shareds;
+using MediatR;
+
+namespace DPTS.Applications.Seller.overviews.Queries
+{
+    public class GetSellerOverviewQuery : IRequest<ServiceResult<IEnumerable<SellerOverviewDto>>>
+    {
+        public string SellerId {  get; set; } = string.Empty;
+        public int CountDay { get; set; } //  7d = 1 week, 30day = month
+    }
+}
