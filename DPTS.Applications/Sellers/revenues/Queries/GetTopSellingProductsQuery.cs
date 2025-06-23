b43792a3551646b61d943fcec7c96209cc92100f
Akai <nguyensieu12112002@gmail.com>
2025-06-23 08:22:49 +0700

    Create GetTopSellingProductsQuery.cs

---
+++ DPTS.Applications/Sellers/revenues/Queries/GetTopSellingProductsQuery.cs
@@ -0,0 +1,11 @@
+﻿using DPTS.Applications.Sellers.revenues.Dtos;
+using DPTS.Applications.Shareds;
+using MediatR;
+
+namespace DPTS.Applications.Sellers.revenues.Queries
+{
+    public class GetTopSellingProductsQuery : IRequest<ServiceResult<IEnumerable<TopSellingProductDto>>>
+    {
+        public string SellerId { get; set; } = string.Empty;
+    }
+}
