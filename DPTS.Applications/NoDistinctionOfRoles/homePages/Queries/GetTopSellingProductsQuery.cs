3216437c37124160361584834580e431a27cb246
Akai <nguyensieu12112002@gmail.com>
2025-06-23 08:23:17 +0700

    Update GetTopSellingProductsQuery.cs

--- DPTS.Applications/NoDistinctionOfRoles/homePages/Queries/GetTopSellingProductsQuery.cs
+++ DPTS.Applications/NoDistinctionOfRoles/homePages/Queries/GetTopSellingProductsQuery.cs
@@ -1,11 +1,6 @@
 ﻿using DPTS.Applications.NoDistinctionOfRoles.homePages.Dtos;
 using DPTS.Applications.Shareds;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DPTS.Applications.NoDistinctionOfRoles.homePages.Queries
 {
