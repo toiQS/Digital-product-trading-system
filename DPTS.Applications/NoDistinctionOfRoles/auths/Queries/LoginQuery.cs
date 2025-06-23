0fb75e318ad6fb63da760d66dfd54bfd43d54266
Akai <nguyensieu12112002@gmail.com>
2025-06-23 08:24:10 +0700

    Create LoginQuery.cs

---
+++ DPTS.Applications/NoDistinctionOfRoles/auths/Queries/LoginQuery.cs
@@ -0,0 +1,12 @@
+﻿using DPTS.Applications.NoDistinctionOfRoles.auths.Dtos;
+using DPTS.Applications.Shareds;
+using MediatR;
+
+namespace DPTS.Applications.NoDistinctionOfRoles.auths.Queries
+{
+    public class LoginQuery : IRequest<ServiceResult<LoginDto>>
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
