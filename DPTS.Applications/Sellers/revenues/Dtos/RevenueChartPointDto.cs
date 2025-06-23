5ae51c93fd9e4ebda7a6a2cbdb166e326cd894bb
Akai <nguyensieu12112002@gmail.com>
2025-06-23 08:23:54 +0700

    Create RevenueChartPointDto.cs

---
+++ DPTS.Applications/Sellers/revenues/Dtos/RevenueChartPointDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DPTS.Applications.Sellers.revenues.Dtos
+{
+    public class RevenueChartPointDto
+    {
+        public DateTime Date { get; set; }
+        public decimal Revenue { get; set; }
+    }
+
+}
