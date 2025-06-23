937273570118c979cf6d7a8ea68b74030aa540c7
Akai <nguyensieu12112002@gmail.com>
2025-06-23 08:23:06 +0700

    Update ProductDetailDto.cs

--- DPTS.Applications/NoDistinctionOfRoles/products/Dtos/ProductDetailDto.cs
+++ DPTS.Applications/NoDistinctionOfRoles/products/Dtos/ProductDetailDto.cs
@@ -1,20 +1,31 @@
-﻿namespace DPTS.Applications.NoDistinctionOfRoles.products.Dtos
+﻿using System.Text;
+
+namespace DPTS.Applications.NoDistinctionOfRoles.products.Dtos
 {
     public class ProductDetailDto
     {
-        public string ProductId { get; set; }
-        public string Name { get; set; }
-        public string SellerId { get; set; }
-        public string SellerName { get; set; }
-        public string Description { get; set; }
+        // Header
+        public string ProductId { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public double Rating { get; set; }
+        public int CountReviews { get; set; }
+        public string StoreImage { get; set; } = string.Empty;
+        public string StoreName { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public decimal? OriginalPrice { get; set; }
-        public bool IsBestseller { get; set; }
-        public int TotalSold { get; set; }
-        public double RatingAverage { get; set; }
-        public int TotalReviews { get; set; }
-        public List<string> Tags { get; set; }
-        public List<string> ImageUrls { get; set; }
-    }
+        public decimal OriginalPrice { get; set; }
+        public int Discount { get; set; }
+        public string Summary { get; set; } = string.Empty;
 
+        public List<string> ProductImage { get; set; } = new();
+
+        // Body
+        public string Description { get; set; } = string.Empty;
+        public decimal Vote1 { get; set; }
+        public decimal Vote2 { get; set; }
+        public decimal Vote3 { get; set; }
+        public decimal Vote4 { get; set; }
+        public decimal Vote5 { get; set; }
+        public List<ProductReviewIndexDto> ProductReviewIndex { get; set; } = new List<ProductReviewIndexDto> { };
+        public List<ProductSuggestIndexDto> ProductSuggestIndex { get; set; } = new List<ProductSuggestIndexDto> { };
+    }
 }
