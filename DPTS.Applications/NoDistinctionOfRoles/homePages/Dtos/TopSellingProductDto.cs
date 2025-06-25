bb26431e80561948f9a01cf53623a4dd09ca373d
Akai <nguyensieu12112002@gmail.com>
2025-06-25 16:26:53 +0700

    Update TopSellingProductDto.cs

--- DPTS.Applications/NoDistinctionOfRoles/homePages/Dtos/TopSellingProductDto.cs
+++ DPTS.Applications/NoDistinctionOfRoles/homePages/Dtos/TopSellingProductDto.cs
@@ -5,7 +5,7 @@
         public string ProductId { get; set; } = string.Empty;
         public string ProductName { get; set; } = string.Empty;
         public decimal Price { get; set; }
-        public double Rating { get; set; }
+        public double RatingOverall { get; set; }
         public int SoldQuantity { get; set; }
         public string ImageUrl { get; set; } = string.Empty;
     }
