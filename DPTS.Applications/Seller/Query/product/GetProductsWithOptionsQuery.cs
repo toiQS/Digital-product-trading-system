00a53369c05f4069e12cc509f17c10626c7fee38
Akai <nguyensieu12112002@gmail.com>
2025-07-18 17:05:25 +0700

    Add GetProductsWithOptions handler and update query
    Introduces GetProductsWithOptionsHandler implementing IRequestHandler for product queries. Extends GetProductsWithOptionsQuery to include filtering and pagination properties for more flexible product retrieval.

--- DPTS.Applications/Seller/Query/product/GetProductsWithOptionsQuery.cs
+++ DPTS.Applications/Seller/Query/product/GetProductsWithOptionsQuery.cs
@@ -1,6 +1,17 @@
-﻿namespace DPTS.Applications.Seller.Query.product
+﻿using DPTS.Applications.Seller.Dtos.product;
+using DPTS.Applications.Shareds;
+using DPTS.Domains;
+using MediatR;
+
+namespace DPTS.Applications.Seller.Query.product
 {
-    public class GetProductsWithOptionsQuery
+    public class GetProductsWithOptionsQuery : IRequest<ServiceResult<ProductListItemDto>>
     {
+        public string Text { get; set; }
+        public string CategoryId { get; set; }
+        public string SellerId { get; set; }
+        public ProductStatus Status { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
     }
 }
