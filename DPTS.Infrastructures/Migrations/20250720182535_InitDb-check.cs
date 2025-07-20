86724938a4794eabb3ad952f220acf34106a1245
Akai <nguyensieu12112002@gmail.com>
2025-07-21 01:27:22 +0700

    fix relationship between message and store

---
+++ DPTS.Infrastructures/Migrations/20250720182535_InitDb-check.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DPTS.Infrastructures.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitDbcheck : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Stores_StoreId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Stores_StoreId1",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_StoreId",
+                table: "Messages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Messages_StoreId1",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "StoreId",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "StoreId1",
+                table: "Messages");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StoreId",
+                table: "Messages",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StoreId1",
+                table: "Messages",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_StoreId",
+                table: "Messages",
+                column: "StoreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_StoreId1",
+                table: "Messages",
+                column: "StoreId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Stores_StoreId",
+                table: "Messages",
+                column: "StoreId",
+                principalTable: "Stores",
+                principalColumn: "store_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Stores_StoreId1",
+                table: "Messages",
+                column: "StoreId1",
+                principalTable: "Stores",
+                principalColumn: "store_id");
+        }
+    }
+}
