86ad4b6dc5ce9591c5089a122be09d8c5b7f7d53
Akai <nguyensieu12112002@gmail.com>
2025-07-24 14:01:08 +0700

    add services get categories for seller

---
+++ DPTS.Applications/Seller/Query/product/GetCategoiesQuery.cs
@@ -0,0 +1,10 @@
+﻿using DPTS.Applications.Seller.Dtos.product;
+using DPTS.Applications.Shareds;
+using MediatR;
+
+namespace DPTS.Applications.Seller.Query.product
+{
+    public class GetCategoiesQuery : IRequest<ServiceResult<CategoryDto>>
+    {
+    }
+}
