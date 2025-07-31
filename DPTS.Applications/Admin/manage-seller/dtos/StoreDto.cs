1aeae27425419aef6dac10e45ad95bdb3848e4cf
Akai <nguyensieu12112002@gmail.com>
2025-07-31 23:22:27 +0700

    up

--- DPTS.Applications/Admin/manage-seller/dtos/StoreDto.cs
+++ DPTS.Applications/Admin/manage-seller/dtos/StoreDto.cs
@@ -2,14 +2,15 @@
 {
     public class StoreDto
     {
+        public int Count { get; set; }
         public List<StoreIndexDto> StoreIndices { get; set; } = new List<StoreIndexDto>();
     }
     public class StoreIndexDto
     {
         public string StoreId { get; set; }
-        public string StoreName { get; set; }   
+        public string StoreName { get; set; }
         public int CountProduct { get; set; }
-        public decimal Revenue { get; set; }  
+        public decimal Revenue { get; set; }
         public double Rating { get; set; }
         public string Status { get; set; }
         public string StoreImage { get; set; }
