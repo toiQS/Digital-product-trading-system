d4a92e0f75e5b19ea03d8e32fa602c3eefd773ea
Akai <nguyensieu12112002@gmail.com>
2025-08-02 00:19:58 +0700

    tạm thời

---
+++ DPTS.Applications/Buyer/Queries/payment/CreatePaymentCommand.cs
@@ -0,0 +1,13 @@
+﻿using DPTS.Applications.Shareds;
+using MediatR;
+
+namespace DPTS.Applications.Buyer.Queries.payment
+{
+    public class CreatePaymentCommand : IRequest<ServiceResult<string>>
+    {
+        public string OrderId { get; set; }
+        public decimal Amount { get; set; }
+        public string BankCode { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
