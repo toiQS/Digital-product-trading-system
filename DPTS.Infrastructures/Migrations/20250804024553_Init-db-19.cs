d8e45529c4ae8e22c2ef47e42d48ee7bae475f0c
Akai <nguyensieu12112002@gmail.com>
2025-08-04 18:24:25 +0700

    Add IsDeleted column to PaymentMethods table
    Introduces a new boolean column 'is_deleted' to the PaymentMethods table to support soft deletion of payment methods. Migration and model snapshot files have been updated accordingly.

---
+++ DPTS.Infrastructures/Migrations/20250804024553_Init-db-19.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DPTS.Infrastructures.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initdb19 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_deleted",
+                table: "PaymentMethods",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_deleted",
+                table: "PaymentMethods");
+        }
+    }
+}
